01045d9df0cad44eb1323eed5d79966df9620888
ehsan <ehsan.s1987@gmail.com>
2019-06-30 03:34:45 +0430

    zigzag conversion - improvement

--- zigzagconversion/src/main/java/Solution.java
+++ zigzagconversion/src/main/java/Solution.java
@@ -3,55 +3,27 @@
  */
 public class Solution {
     public String convert(String s, int numRows) {
-        Group[] groups = Group.makeGroups(s, numRows);
-        String[] rows = new String[numRows];
-        for (int i = 0; i < numRows; ++i) {
-            rows[i] = "";
+        if(s.length() < numRows || numRows == 1) {
+            return s;
         }
-        for (int i = 0; i < groups.length; ++i) {
-            for (int r = 0; r < numRows; ++r) {
-                rows[r] += groups[i].getRowAt(r);
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < numRows; ++i) {
+            int index = i;
+            while(index < s.length()) {
+                int pivot = getNextPivot(index, numRows);
+                result.append(s.charAt(index));
+                index = mirror(index, pivot);
             }
         }
-        StringBuilder resultBuilder = new StringBuilder("");
-        for (int i = 0; i < numRows; ++i) {
-            resultBuilder = resultBuilder.append(rows[i]);
-        }
-        return resultBuilder.toString();
+        return result.toString();
     }
 
-    public static class Group {
-        private String[] rows;
+    private int mirror(int index, int pivot) {
+        return 2 * pivot - index;
+    }
 
-        public String getRowAt(int i) {
-            return rows[i];
-        }
-
-        public Group(String input, int numRows) {
-            this.rows = new String[numRows];
-            for (int i = 0; i < numRows; ++i) {
-                rows[i] = "";
-            }
-            for (int i = 0; i < Math.min(numRows, input.length()); ++i) {
-                rows[i] += input.charAt(i);
-            }
-
-            int j = numRows;
-            for (int i = numRows - 1; i > 1 && j < input.length(); --i) {
-                rows[i - 1] += input.charAt(j++);
-            }
-        }
-
-        public static Group[] makeGroups(String input, int numRows) {
-            int groupSize = numRows * 2 - 2;
-            int numGroups = (int) Math.ceil((double) input.length() / groupSize);
-            Group[] groups = new Group[numGroups];
-            for (int i = 0; i < numGroups; i++) {
-                int beginIndex = i * groupSize;
-                int endIndex = Math.min((i + 1) * groupSize, input.length());
-                groups[i] = new Group(input.substring(beginIndex, endIndex), numRows);
-            }
-            return groups;
-        }
+    private int getNextPivot(int index, int numRows) {
+        return (numRows - 1) * (index / (numRows- 1) + 1);
     }
 }
